872512c562c45d3fc5615ce9a50bc11d13d0578f
Jules Sainthorant <jsaintho@bess-f3r3s1.clusters.42paris.fr>
2024-05-14 12:21:47 +0200

    f commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME = fractol
+SRCS_ = fractol.c
+SRCS_OBJ = $(SRCS:.c=.o)
+
+CFLAGS = -Wall
+
+UNAME =$(shell uname -s)
+ifeq ($(shell uname -s), Linux)
+	INCLUDES = -I/usr/include -Imlx
+else
+	INCLUDES = -I/opt/X11/include -Imlx
+endif
+ 
+MLX_DIR = ./mlx
+MLX_LIB = $(MLX_DIR)/libmlx_$(UNAME).a
+
+ifeq ($(shell uname -s), Linux)
+	MLX_FLAGS = -Lmlx -lmlx -L/usr/lib/X11 -lXext -lX11
+else
+	MLX_FLAGS = -Lmlx -lmlx -L/usr/X11/lib -lXext -lX11 -framework OpenGL -framework AppKit
+endif
+ 
+
+ 
+all: $(MLX_LIB) $(NAME)
+ 
+.c.o:
+	gcc $(CFLAGS) -c -o $@ $< $(INCLUDES)
+ 
+$(NAME): $(SRCS_OBJ)
+	gcc $(CFLAGS) fractol.c $(INCLUDES) -o $(NAME) $(SRCS_OBJ) $(MLX_FLAGS)
+ 
+$(MLX_LIB):
+	@make -C $(MLX_DIR)
+
+clean:
+	rm -f $(SRCS_OBJ)
+
+.PHONY: clean all
---
+++ fractol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaintho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 10:35:22 by jsaintho          #+#    #+#             */
+/*   Updated: 2024/05/14 12:19:25 by jsaintho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <mlx.h>
+#include <math.h>
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
